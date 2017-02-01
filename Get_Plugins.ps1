1bb44cfd90e6d5352148effac34cae38bf5af7fd
jrwhite17 <johnrileywhite@gmail.com>
2017-01-31 20:55:04 -0600

    Update 1
    Downloads every version of every plugin.

--- Get_Plugins.ps1
+++ Get_Plugins.ps1
@@ -1,22 +1,42 @@
 $URL = "https://updates.jenkins-ci.org/download/plugins/"
 
-$CurrentDirectory = (Get-Item -Path ".\" -Verbose).FullName
-$CurrentDate = (Get-Date).AddDays(-1).ToString('yyyy-MM-dd')
-$DOWNLOAD_PATH = $CurrentDirectory+"\PluginPacks\"+$CurrentDate
+$CURRENT_DIRECTORY = (Get-Item -Path ".\" -Verbose).FullName
+$CURRENT_DATE = (Get-Date).ToString('yyyy-MM-dd')
+$DOWNLOAD_PATH = $CURRENT_DIRECTORY+"\PluginPacks\"+$CURRENT_DATE
 
 #Create directory
 New-Item -ItemType Directory -Force -Path $DOWNLOAD_PATH
 
-#Go to that directory
-cd $DOWNLOAD_PATH
-
 #Get HTML
-$HTML = Invoke-WebRequest -URI $URL
+$PLUGINS_HTML = Invoke-WebRequest -URI $URL
 
-foreach($PLUGIN in $HTML.Links.innerHTML){
-	$PLUGIN_FILE_NAME = $PLUGIN.Substring(0,$PLUGIN.Length-1)+".hpi"
-	$PLUGIN_URL = "https://updates.jenkins-ci.org/latest/"+$PLUGIN_FILE_NAME
+foreach($PLUGIN in $PLUGINS_HTML.Links.innerHTML){
+	$PLUGIN_FILE_NAME = $PLUGIN.Substring(0,$PLUGIN.Length-1)
+	$PLUGIN_URL = $URL+$PLUGIN_FILE_NAME
 	write-output $PLUGIN_URL
+	#Create directory
+	New-Item -ItemType Directory -Force -Path $DOWNLOAD_PATH\$PLUGIN_FILE_NAME
 	
-	Invoke-WebRequest -Uri $PLUGIN_URL -OutFile $PLUGIN_FILE_NAME
+	$PLUGIN_VERSION_HTML = Invoke-WebRequest -Uri $PLUGIN_URL
+	foreach($PLUGIN_VERSION in $PLUGIN_VERSION_HTML.Links.innerHTML){
+	
+		if($PLUGIN_VERSION.equals("permalink to the latest")){
+			$PLUGIN_VERSION = "latest"
+			$PLUGIN_VERSION_URL = "https://updates.jenkins-ci.org/"+$PLUGIN_VERSION+"/"+$PLUGIN_FILE_NAME+".hpi"
+			#Example - https://updates.jenkins-ci.org/latest/BlazeMeterJenkinsPlugin.hpi
+		}else{
+			$PLUGIN_VERSION_URL = $URL+$PLUGIN_FILE_NAME+"/"+$PLUGIN_VERSION+"/"+$PLUGIN_FILE_NAME+".hpi"
+			#Example - https://updates.jenkins-ci.org/download/plugins/BlazeMeterJenkinsPlugin/2.7/BlazeMeterJenkinsPlugin.hpi
+		}
+	
+		#Create directory
+		New-Item -ItemType Directory -Force -Path $DOWNLOAD_PATH\$PLUGIN_FILE_NAME\$PLUGIN_VERSION
+		
+		#write-output $PLUGIN_VERSION
+		Invoke-WebRequest -Uri $PLUGIN_VERSION_URL -OutFile $DOWNLOAD_PATH\$PLUGIN_FILE_NAME\$PLUGIN_VERSION\$PLUGIN_FILE_NAME".hpi"
+	
+	}
+	
+	
+	
 }
